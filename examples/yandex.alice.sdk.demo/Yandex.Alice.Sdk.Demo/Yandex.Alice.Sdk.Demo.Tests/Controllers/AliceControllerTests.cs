f10dcc4df09ccbc086f93eca3bc685796295a192
alexvolchetsky <alex.volchetsky@gmail.com>
2020-07-01 17:56:44 +0300

    create project and test request

---
+++ examples/yandex.alice.sdk.demo/Yandex.Alice.Sdk.Demo/Yandex.Alice.Sdk.Demo.Tests/Controllers/AliceControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using Yandex.Alice.Sdk.Demo.Tests.TestsInfrastructure;
+using Yandex.Alice.Sdk.Models;
+
+namespace Yandex.Alice.Sdk.Demo.Tests.Controllers
+{
+    public class AliceControllerTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public AliceControllerTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task TestAlice()
+        {
+            var hostBuilder = Program.CreateHostBuilder(Array.Empty<string>())
+                .ConfigureWebHost(webhost => webhost.UseTestServer());
+            var host = await hostBuilder.StartAsync();
+            var client = host.GetTestClient();
+
+            string json = File.ReadAllText(TestsConstants.AliceRequestFilePath);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("alice", content).ConfigureAwait(false);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            string responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            _testOutputHelper.WriteLine(responseContent);
+        }
+    }
+}
---
+++ examples/yandex.alice.sdk.demo/Yandex.Alice.Sdk.Demo/Yandex.Alice.Sdk.Demo.Tests/TestsInfrastructure/TestsConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yandex.Alice.Sdk.Demo.Tests.TestsInfrastructure
+{
+    static class TestsConstants
+    {
+        public const string AliceRequestFilePath = "TestsInfrastructure/Assets/AliceRequest.json";
+    }
+}
