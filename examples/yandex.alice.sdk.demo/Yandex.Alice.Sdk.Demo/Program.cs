eeca6b1b0310ea6d1d778905e4b3d023cb679da6
alexvolchetsky <alex.volchetsky@gmail.com>
2020-12-12 12:42:22 +0300

    update demo project to net5.0

--- examples/yandex.alice.sdk.demo/Yandex.Alice.Sdk.Demo/Program.cs
+++ examples/yandex.alice.sdk.demo/Yandex.Alice.Sdk.Demo/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+[assembly:CLSCompliant(false)]
 namespace Yandex.Alice.Sdk.Demo
 {
     public static class Program
