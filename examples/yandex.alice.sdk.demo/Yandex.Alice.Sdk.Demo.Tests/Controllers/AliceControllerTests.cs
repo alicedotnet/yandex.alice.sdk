9447c0b5df9ca1a0bc13ff379c87ca315624cf7b
alexvolchetsky <alex.volchetsky@gmail.com>
2020-12-13 18:56:55 +0300

    cleanup

--- examples/yandex.alice.sdk.demo/Yandex.Alice.Sdk.Demo.Tests/Controllers/AliceControllerTests.cs
+++ examples/yandex.alice.sdk.demo/Yandex.Alice.Sdk.Demo.Tests/Controllers/AliceControllerTests.cs
@@ -1,16 +1,10 @@
-﻿using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.IO;
+﻿using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
-using Yandex.Alice.Sdk.Demo.Services.Interfaces;
 using Yandex.Alice.Sdk.Demo.Tests.TestsInfrastructure;
 using Yandex.Alice.Sdk.Demo.Tests.TestsInfrastructure.Fixtures;
 
@@ -21,20 +15,18 @@ namespace Yandex.Alice.Sdk.Demo.Tests.Controllers
     {
         private readonly ITestOutputHelper _testOutputHelper;
         private readonly HttpClient _client;
-        private readonly ICleanService _cleanService;
 
         public AliceControllerTests(ITestOutputHelper testOutputHelper, TestServerFixture testServerFixture)
         {
             _testOutputHelper = testOutputHelper;
             _client = testServerFixture.DemoClient;
-            _cleanService = testServerFixture.Services.GetRequiredService<ICleanService>();
         }
 
         [Theory]
-        [InlineData(TestsConstants.AliceRequestFilePath, false)]
-        [InlineData(TestsConstants.AliceRequestInvalidIntentFilePath, false)]
-        [InlineData(TestsConstants.AliceRequestPingFilePath, false)]
-        public async Task TestAlice(string filePath, bool cleanResources)
+        [InlineData(TestsConstants.AliceRequestFilePath)]
+        [InlineData(TestsConstants.AliceRequestInvalidIntentFilePath)]
+        [InlineData(TestsConstants.AliceRequestPingFilePath)]
+        public async Task TestAlice(string filePath)
         {
             string json = File.ReadAllText(filePath);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
@@ -43,11 +35,6 @@ namespace Yandex.Alice.Sdk.Demo.Tests.Controllers
             Assert.True(HttpStatusCode.OK == response.StatusCode, responseContent);
 
             _testOutputHelper.WriteLine(responseContent);
-
-            if(cleanResources)
-            {
-                await _cleanService.CleanResourcesAsync().ConfigureAwait(false);
-            }
         }
     }
 }
