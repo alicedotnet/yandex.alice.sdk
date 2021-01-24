a970fe278ce7b63566e0cc47d4dc6e897067c46a
alexvolchetsky <alex.volchetsky@gmail.com>
2021-01-24 23:54:37 +0300

    initialize buttons with empty collection to avoid nullreferenceexception

--- src/Yandex.Alice.Sdk/Models/AliceResponse.cs
+++ src/Yandex.Alice.Sdk/Models/AliceResponse.cs
@@ -8,12 +8,12 @@ namespace Yandex.Alice.Sdk.Models
     public class AliceResponse : AliceResponse<object, object>
     {
         public AliceResponse(AliceRequestBase request, string text)
-            : this(request, text, text, null)
+            : this(request, text, text, new List<AliceButtonModel>())
         {
         }
 
         public AliceResponse(AliceRequestBase request, string text, string tts)
-            : this(request, text, tts, null)
+            : this(request, text, tts, new List<AliceButtonModel>())
         {
         }
 
@@ -33,12 +33,12 @@ namespace Yandex.Alice.Sdk.Models
        : AliceResponseBase<AliceResponseModel, TSession, TUser>
     {
         public AliceResponse(AliceRequestBase request, string text)
-            : this(request, text, text, null)
+            : this(request, text, text, new List<AliceButtonModel>())
         {
         }
 
         public AliceResponse(AliceRequestBase request, string text, string tts)
-            : this(request, text, tts, null)
+            : this(request, text, tts, new List<AliceButtonModel>())
         {
         }
 
