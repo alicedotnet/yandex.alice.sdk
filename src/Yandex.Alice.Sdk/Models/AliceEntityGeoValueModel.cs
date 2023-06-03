7b2ef91ef5a5d94837e4a8cbae5b915637147213
Anatoliy Savinov <anatoly.s.savinov@gmail.com>
2023-06-03 18:39:34 +0300

    Added Country and City to AliceEntityGeoValueModel

--- src/Yandex.Alice.Sdk/Models/AliceEntityGeoValueModel.cs
+++ src/Yandex.Alice.Sdk/Models/AliceEntityGeoValueModel.cs
@@ -6,6 +6,12 @@
     [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
     public class AliceEntityGeoValueModel
     {
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+
         [JsonPropertyName("house_number")]
         public string HouseNumber { get; set; }
 
